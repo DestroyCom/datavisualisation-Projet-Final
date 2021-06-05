f0a7e1a7e69c0ae749bf1f45d5ea529112e1d7e4
DestCom <azevedo-a@hotmail.fr>
2021-06-05 17:33:17 +0200

    Finalisation du scraper SetList + Data beatle & RS

--- bots/setList-Scraper/main.py
+++ bots/setList-Scraper/main.py
@@ -36,6 +36,9 @@ nomPays = driver.find_elements_by_css_selector('.songName>a>span')
 placeDuPays = driver.find_elements_by_css_selector('.countId')
 nombreDeConcert = driver.find_elements_by_css_selector('.barChart>span')
 
+#get the page URL for later
+getURL = driver.current_url
+
 #Initialize some var
 datas = []
 i = 0
@@ -48,7 +51,7 @@ for pays in nomPays:
     data.append(nombreDeConcert[i].text)
     i = i + 1
     datas.append(data)
-  
+ 
 #Write the csv file
 with open(str(who), 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
@@ -57,6 +60,54 @@ with open(str(who), 'w', encoding='UTF8', newline='') as f:
     writer.writerows(datas)
 
     f.close()
+
+
+#Selectionne le nombre d'année cliquable & leurs texte
+selectYear = driver.find_elements_by_css_selector('.artistplayCounts div>ul>li>a:nth-child(1)')
+selectYearText = driver.find_elements_by_css_selector('.artistplayCounts div>ul>li>a:nth-child(1)>span')
+
+#Donne la date de départ + 1
+dateStarted = int(selectYearText[0].text) + 1
+
+#calcul le nombre de concert du grp
+numberOfYears = int(selectYearText[0].text ) - int(selectYearText[len(selectYearText) - 1].text)
+
+#Ajoute les data pour chaque année
+for h in range(numberOfYears+1):
+    #Change l'année selectionné
+    dateStarted = dateStarted - 1
+    newPlace = getURL + '?year=' + str(dateStarted)
+    driver.get(str(newPlace)) 
+   
+    nomPaysDate = driver.find_elements_by_css_selector('.songName>a>span')
+    placeDuPaysDate = driver.find_elements_by_css_selector('.countId')
+    nombreDeConcertDate = driver.find_elements_by_css_selector('.barChart>span')
+    
+    #Initialize some var
+    datasDate = []
+    y = 0
+
+    #Get the data into a list
+    for pays in nomPaysDate:
+        dataDate = []
+        dataDate.append(y + 1)
+        dataDate.append(pays.text)
+        dataDate.append(nombreDeConcertDate[y].text)
+        y = y + 1
+        datasDate.append(dataDate)
+    
+    whoDate = './bots/setList-Scraper/data/' + str(dateStarted) + '-' + where + '-SetList.csv'
+
+    #Write the csv file for the date
+    with open(str(whoDate), 'w', encoding='UTF8', newline='') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(header)
+        writer.writerows(datasDate)
+
+        f.close()
+    
+    
 
 #Close the firefox tab & display a message
 driver.close()
