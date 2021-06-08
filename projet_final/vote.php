3f24c4a2df1617341bd7f0ff5cc6f73dd61241ce
Stanley.js <76979070+Stan-Jms@users.noreply.github.com>
2021-06-08 10:44:13 +0200

    Add files via upload
    someshit

---
+++ projet_final/vote.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+            $group1 = $_GET['group1'];
+            $group2 = $_GET['group2'];
+        ?>
+        <meta charset="utf-8">
+        <title>Vote</title>
+        <style type="text/css">
+            body {
+            height: 98vh;
+            width: 98vw;
+            background:white;
+            }
+            .bg_cover > .veil {
+                height: 98vh;
+                width: 98vw;
+                background: black;
+                opacity: 0.35;
+                z-index: 3;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="bg_cover">
+            <img src="img/<?=htmlspecialchars($group1);?>_cover.png"></img>
+            <img src="img/<?=htmlspecialchars($group2);?>_cover.png"></img>
+            <div class="veil">
+            </div>
+        </div>
+        <div class="faces">
+            <img src="img/<?=htmlspecialchars($group1);?>_faces.png"></img>
+            <img src="img/<?=htmlspecialchars($group2);?>_faces.png"></img>
+        </div>
+        <div class="titles">
+            <p><?= htmlspecialchars($group1);?></p>
+            <img src="img/vs.png"></img>
+            <p><?= htmlspecialchars($group2);?></p>
+        </div>
+        <div class="buttons">
+            <button>Vote1</button>
+            <button>Vote2</button>
+            <button>see data</button>
+        </div>
+
+    </body>
+</html>
