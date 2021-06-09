e1889f1341c6359472492077340e22f855b62006
DestCom <azevedo-a@hotmail.fr>
2021-06-09 15:54:34 +0200

    fixes

--- credentials/credentials.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $user = 'rock-band_db';
-    $pass = 'dataVizProjet2021';
-
-    $pdo = new PDO('mysql:host=mysql-rock-band.alwaysdata.net;dbname=rock-band_db', $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-?>
--- credentials/lastfm.js
+++
@@ -1,3 +0,0 @@
-var apiKey = 'd7c19e822e436a0ee056a6a73f71b04d';
-
-export {apiKey};
