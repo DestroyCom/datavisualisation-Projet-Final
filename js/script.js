829788282f0981c00e588d2afd41f1c22b1b296a
Adrien Albuquerque <adrialb95@gmail.com>
2021-06-10 19:06:23 +0200

    script js data.php

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.getElementById("stream").addEventListener('click', function(){
+    document.getElementById("stream").style.backgroundColor="#353131";
+    document.getElementById("concert").style.backgroundColor="#8F8F8F";
+    document.getElementById("PageStream").classList.remove("hidden");
+    document.getElementById("PageMap").classList.add("hidden");
+    document.getElementById("Calque_1").classList.add("hidden");
+  });
+
+document.getElementById("concert").addEventListener('click', function(){
+    document.getElementById("stream").style.backgroundColor="#8F8F8F";
+    document.getElementById("concert").style.backgroundColor="#353131";
+    document.getElementById("PageStream").classList.add("hidden");
+    document.getElementById("PageMap").classList.remove("hidden");
+    document.getElementById("Calque_1").classList.remove("hidden");
+  });
