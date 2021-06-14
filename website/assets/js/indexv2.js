9a6b8f0493b583043aaf654d958ace62157d29c9
ADR1811 <adrialb95@gmail.com>
2021-06-14 16:40:39 +0200

    vs select menu

--- website/assets/js/indexv2.js
+++ website/assets/js/indexv2.js
@@ -1,5 +1,6 @@
 let i=1;
 var tableau=[];
+var tabSelect=[]
 while(i<=12){
     tableau[i]=document.getElementById("g"+i);
     i=i+1;
@@ -8,6 +9,24 @@ while(i<=12){
 
 tableau.forEach(function(item,index){
     item.addEventListener("click",function(){
-        item.style.backgroundColor="red";
+        
+        if (tabSelect.length<2){
+            item.style.backgroundColor="red";
+            tabSelect.push(item);
+            
+
+        }
+        else if(tabSelect.length=2){
+            tabSelect[0].style.backgroundColor="#000000";
+            tabSelect[0]=tabSelect[1];
+            tabSelect[1]=item;
+            item.style.backgroundColor="red";
+            
+            
+        }
     })
 });
+
+document.getElementById("go").addEventListener("click",function(){
+    window.location="data.php?groupe1="+tabSelect[0].id.replace("g","")+"&groupe2="+tabSelect[1].id.replace("g","");
+})
