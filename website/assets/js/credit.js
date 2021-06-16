e924728788284dedc97c72a5bfdc77d59f9f7bcc
ThomAzgo <weifang.buisson@gmail.com>
2021-06-16 16:33:49 +0200

    derniers changements js (probablement pas)

--- website/assets/js/credit.js
+++ website/assets/js/credit.js
@@ -1,16 +1,3 @@
-document.querySelector('.buttonMove').addEventListener('click', function(e){
-    document.querySelector('#contenu1').classList.toggle('hidden');
-    document.querySelector('#contenu2').classList.toggle('hidden');
-})
-document.querySelector('.buttonMove2').addEventListener('click', function(e){
-    document.querySelector('#contenu1').classList.toggle('hidden');
-    document.querySelector('#contenu2').classList.toggle('hidden');
-})
-
 document.querySelector('.fleche').addEventListener('click', function(e){
-    window.location = 'index.html';
-})
-
-document.querySelector('.fleche2').addEventListener('click', function(e){
     window.location = 'index.html';
 })
